68f33b3043d098a0f35ea466e46330b60bc30a95
Bri Dascher <bri-dash@users.noreply.github.com>
2017-04-13 09:54:01 -0600

    assignment5

---
+++ 5_ripples/server.js
@@ -0,0 +1,52 @@
+// HTTP PORTION
+
+var http = require('http');
+var fs = require('fs');
+var httpServer = http.createServer(requestHandler);
+var url = require('url');
+httpServer.listen(8080);
+
+function requestHandler(req, res) {
+
+	var parsedUrl = url.parse(req.url);
+	console.log("The Request is: " + parsedUrl.pathname);
+		
+	fs.readFile(__dirname + parsedUrl.pathname, 
+		function (err, data) {
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading ' + parsedUrl.pathname);
+			}
+			res.writeHead(200);
+			res.end(data);
+  		}
+  	);
+  	
+}
+
+
+// WEBSOCKET PORTION
+
+var io = require('socket.io').listen(httpServer);
+
+setInterval(function() {
+  	io.sockets.emit('clear');
+}, 30000 );
+
+io.sockets.on('connection', 
+
+	function (socket) {
+	
+		console.log("We have a new client: " + socket.id);
+		
+		///MY SOCKET EVENTS HERE
+		socket.on('newmouse', function(data){
+			socket.broadcast.emit('receivedmouse', data);
+		});
+
+
+		socket.on('disconnect', function() {
+			console.log("Client has disconnected " + socket.id);
+		});
+	}
+);
